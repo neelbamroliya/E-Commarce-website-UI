39419fcf4f4f361a77a8cdd4ef1588dd35760741
Neel bamroliya <nbamroliya40@gmail.com>
2022-06-22 23:13:50 +0530

    Cart page design completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
-// import Home from "./pages/Home"
-// import Product from "./pages/Product";
-// import ProductList from "./pages/ProductList";
+import Home from "./pages/Home"
+import Product from "./pages/Product";
+import ProductList from "./pages/ProductList";
+import Cart from "./pages/Cart";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
 
 const App = () => {
-  return <Login />;
+  return <Cart />;
 };
 
 export default App;
